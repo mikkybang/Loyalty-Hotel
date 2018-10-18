dee59fdd04e6b8b55cec3236bd12102a36db50da
root <root@localhost.localdomain>
2018-10-18 09:19:15 +0100

    changed about, contact, hotel and index html files to php files... added the header and footer files to them

---
+++ about.php
@@ -0,0 +1,63 @@
+<?php 
+include ('header.html');
+?>
+
+    <section class="site-hero overlay page-inside" style="background-image: url(img/hero_2.jpg)">
+      <div class="container">
+        <div class="row site-hero-inner justify-content-center align-items-center">
+          <div class="col-md-10 text-center">
+            <h1 class="heading" data-aos="fade-up">About Us</h1>
+            <p class="sub-heading mb-5" data-aos="fade-up" data-aos-delay="100">Enjoy your stay.</p>
+          </div>
+        </div>
+        <!-- <a href="#" class="scroll-down">Scroll Down</a> -->
+      </div>
+    </section>
+    <!-- END section -->
+
+    
+
+    <section class="section slider-section">
+      <div class="container">
+        <div class="row justify-content-center text-center mb-5">
+          <div class="col-md-8">
+            <h2 class="heading" data-aos="fade-up">Hotel Gallery</h2>
+            <p class="lead" data-aos="fade-up" data-aos-delay="100">Lorem ipsum dolor sit amet, consectetur adipisicing elit. In dolor, iusto doloremque quo odio repudiandae sunt eveniet? Enim facilis laborum voluptate id porro, culpa maiores quis, blanditiis laboriosam alias. Sed.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="home-slider major-caousel owl-carousel mb-5" data-aos="fade-up" data-aos-delay="200">
+              <div class="slider-item">
+                <img src="img/slider-1.jpg" alt="Image placeholder" class="img-fluid">
+              </div>
+              <div class="slider-item">
+                <img src="img/slider-2.jpg" alt="Image placeholder" class="img-fluid">
+              </div>
+              <div class="slider-item">
+                <img src="img/slider-3.jpg" alt="Image placeholder" class="img-fluid">
+              </div>
+              <div class="slider-item">
+                <img src="img/slider-4.jpg" alt="Image placeholder" class="img-fluid">
+              </div>
+              <div class="slider-item">
+                <img src="img/slider-5.jpg" alt="Image placeholder" class="img-fluid">
+              </div>
+              <div class="slider-item">
+                <img src="img/slider-6.jpg" alt="Image placeholder" class="img-fluid">
+              </div>
+            </div>
+            <!-- END slider -->
+          </div>
+
+          <div class="col-md-12 text-center"><a href="#" class="">View More Photos</a></div>
+        
+        </div>
+      </div>
+    </section>
+    <!-- END section -->
+    
+    
+    <?php 
+    include ('footer.html');
+    ?>
